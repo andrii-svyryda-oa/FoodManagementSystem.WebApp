755a3823ad5dd0744414f0336097069632d5216d
Андрій Свирида <andrijsvirida2@gmail.com>
2024-12-15 22:43:51 +0200

    Moved login card to component

--- src/app/(not-authorized)/auth/login/page.tsx
+++ src/app/(not-authorized)/auth/login/page.tsx
@@ -1,64 +1,7 @@
 "use client";
 
-import { useLoginMutation } from "@/store/api/auth.api";
-import { LoginPayload } from "@/types/auth";
-import { Button, Card, Form, Input } from "antd";
-import { useForm } from "antd/es/form/Form";
-import useNotification from "antd/es/notification/useNotification";
-import { useEffect } from "react";
+import { LoginCard } from "@/components/auth/logic-card";
 
 export default function LoginPage() {
-  const [notification, contextHolder] = useNotification();
-
-  const [login, { error, isLoading }] = useLoginMutation();
-
-  useEffect(() => {
-    if (error) {
-      notification.error({ message: "Email or password incorrect" });
-    }
-  }, [error]);
-
-  const [form] = useForm<LoginPayload>();
-
-  return (
-    <div>
-      {contextHolder}
-      <Card title="Login" bordered={false} style={{ width: 300 }}>
-        <Form
-          requiredMark={false}
-          name="login"
-          form={form}
-          onFinish={login}
-          layout="vertical"
-        >
-          <Form.Item
-            label="Email"
-            name="email"
-            required
-            rules={[
-              { required: true, message: "Please input your email!" },
-              { type: "email", message: "Please enter a valid email address!" },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-
-          <Form.Item
-            label="Password"
-            name="password"
-            required
-            rules={[{ required: true, message: "Please input your password!" }]}
-          >
-            <Input.Password />
-          </Form.Item>
-
-          <Form.Item>
-            <Button loading={isLoading} type="primary" htmlType="submit" block>
-              Login
-            </Button>
-          </Form.Item>
-        </Form>
-      </Card>
-    </div>
-  );
+  return <LoginCard />;
 }
